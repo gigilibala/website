d92a3c567d4cca42a18f2e38ca2f79dbf17a3c92
Amin Hassani <gigilibala4@gmail.com>
2023-05-03 00:36:02 +0800

    Add simple sidebar

---
+++ src/components/Layout.tsx
@@ -0,0 +1,11 @@
+import SideBar from "@/components/SideBar";
+import { PropsWithChildren } from "react";
+
+export default function Layout({ children }: PropsWithChildren) {
+  return (
+    <div className="relative md:flex min-h-screen">
+      <SideBar />
+      <div className="flex-1 bg-green-50">{children}</div>
+    </div>
+  );
+}
