829fe112fde7fb3faad0f26a9d001c7da74d53b5
Amin Hassani <gigilibala4@gmail.com>
2023-05-19 11:41:04 +0800

    temporarily replace Home page with About page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,11 @@
 import PageHead from '@/components/PageHead'
-import { loremIpsum } from 'lorem-ipsum'
-import { useEffect, useState } from 'react'
+import About from './about'
 
 export default () => {
-  const [content, setContent] = useState('')
-  useEffect(() => {
-    setContent(loremIpsum({ count: 10, units: 'paragraphs' }))
-  }, [])
-
   return (
     <main>
       <PageHead />
-      <div className='max-w-xl text-center'>
-        <div className='w-full h-full mx-auto flex flex-col justify-center items-center'>
-          <p>
-            My name is Amin Hassani! I am a software engineer based on San
-            Francisco bay area. I love building and learning new things. Hit me
-            up if you have anything in mind.
-          </p>
-          {/* <p>{content}</p> */}
-        </div>
-      </div>
+      <About />
     </main>
   )
 }
