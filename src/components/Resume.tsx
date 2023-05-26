aa2bdadead0ff098f8f7500a1ccb49a4c17cf8b4
Amin Hassani <gigilibala4@gmail.com>
2023-05-26 15:30:28 +0330

    Add resume page

---
+++ src/components/Resume.tsx
@@ -0,0 +1,11 @@
+export default function Resume() {
+  return (
+    <div>
+      <iframe
+        src='AminHassani_Resume.pdf#view=FitH&navpanes=0'
+        width={800}
+        height={1000}
+      ></iframe>
+    </div>
+  )
+}
---
+++ src/pages/resume/index.tsx
@@ -0,0 +1,11 @@
+import PageHead from '@/components/PageHead'
+import Resume from '@/components/Resume'
+
+export default () => {
+  return (
+    <main>
+      <PageHead />
+      <Resume />
+    </main>
+  )
+}
