6c351e2298909b9d4daa05a305cdf04748a124cf
Amin Hassani <gigilibala4@gmail.com>
2023-12-02 09:58:16 -0500

    Add a bunch of new updates
    Specially to impossible list and return back the home page.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,11 @@
 export default function Home() {
   return (
-    <div className='p-10 max-w-3xl items-center first-letter:text-5xl'>
+    <div className='p-10 max-w-3xl items-center'>
       <div>
         <p>
-          My name is Amin Hassani! I am a software engineer based on San
-          Francisco bay area. I love building and learning new things. Hit me up
-          if you have anything in mind.
+          Hi there! My name is Amin Hassani! I am a software engineer based on
+          San Francisco bay area. I love learning and building new things. Hit
+          me up if you have anything in mind.
         </p>
       </div>
     </div>
