ab0a7a78e0ada9252786a35eaddc958e98caccf6
Amin Hassani <gigilibala4@gmail.com>
2024-01-08 08:58:43 -0500

    Add firebase config
    Hopefully with this we get the analytics working.

---
+++ firebase.config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDzRCEYcdpEIBQzLR6bEyDUK3jAOyazbtI',
+  authDomain: 'personal-website-c9eaf.firebaseapp.com',
+  projectId: 'personal-website-c9eaf',
+  storageBucket: 'personal-website-c9eaf.appspot.com',
+  messagingSenderId: '654408999060',
+  appId: '1:654408999060:web:d6c78dd0068fc4dfdb9481',
+  measurementId: 'G-6G1E0CZ9RF',
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,10 @@
 import Home from '@/components/Home'
 import PageHead from '@/components/PageHead'
+import { initializeApp } from 'firebase/app'
+import { firebaseConfig } from '../../firebase.config'
+
+// Initialize Firebase.
+initializeApp(firebaseConfig)
 
 export default () => {
   return (
