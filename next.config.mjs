c88ba5fac2295b74eb49f84ab0cb96ca716b9359
Amin Hassani <15792044+gigilibala@users.noreply.github.com>
2024-08-27 12:10:00 -0700

    feat(website): add semantic-release (#56)

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,14 @@
+import packageJson from './package.json' with { type: 'json' }
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
   images: {
     unoptimized: true,
+  },
+  env: {
+    APP_VERSION: packageJson.version,
   },
 }
 
