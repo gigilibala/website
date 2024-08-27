adf93887adcad82911176271007e8683108c7838
Amin Hassani <gigilibala4@gmail.com>
2024-08-27 16:33:12 -0700

    fix(release): change package.json

--- releaseExec.mjs
+++ releaseExec.mjs
@@ -5,7 +5,9 @@ import packageJson from './package.json' with { type: 'json' }
 async function prepare(_, context) {
   const next = context.nextRelease.version
   packageJson.version = next
-  writeFileSync('./package.json', JSON.stringify(packageJson, null, 2))
+  writeFileSync('./package.json', JSON.stringify(packageJson, null, 2) + '\n', {
+    flush: true,
+  })
 
   const output = execSync('yarn build')
   console.log(output.toString())
